ce50088cbdb9634addeaff2e656524cf89d73a88
qny <2428236063@qq.com>
2020-11-26 02:36:35 +0800

    add

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import './plugins/element.js'
 import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import TreeTable from 'vue-table-with-tree-grid'
+//导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+//导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 import axios from 'axios'
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
@@ -16,6 +23,8 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.component('tree-table', TreeTable)
+//将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
 
 new Vue({
   router,
